9e939315ad26da5c7c6345cbbda299d889aedca3
Srikanth <bhuthamsrikanth123@gmail.com>
2025-12-12 01:48:35 +0530

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import List
 import pickle
 import numpy as np
 app = FastAPI()
